b309796639d841c06f3f25cbc0ba373c62f1ed6c
Ali Shahidi <alishahidi1376@gmail.com>
2024-08-21 11:51:37 +0330

    fix

--- src/main/java/com/example/hibernate/config/RedisConfig.java
+++ src/main/java/com/example/hibernate/config/RedisConfig.java
@@ -3,17 +3,24 @@ package com.example.hibernate.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("redis.host")
+    public String host;
+
+    @Value("redis.port")
+    public String port;
+
     @Bean(destroyMethod = "shutdown")
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress("redis://redis:6379");
+                .setAddress("redis://" + host + ":" + port);
         return Redisson.create(config);
     }
 }
